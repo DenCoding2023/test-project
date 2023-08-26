b3e0157477334f2d6bb321adf546d670693b3446
Dennis Luciano <127637377+DenCoding2023@users.noreply.github.com>
2023-08-26 13:14:35 -0400

    update the navigation

--- client/src/components/Nav/index.jsx
+++ client/src/components/Nav/index.jsx
@@ -23,6 +23,7 @@ function Nav() {
         // </ul>
         <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
         <a class="navbar-brand" href="#">Home</a>
+        {/*<Link class="navbar-brand" to="/">Home</Link>*/} {/* Link to the home page */}
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
